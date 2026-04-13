f11cce185324696abb4f98ae38041fc1b3d14932
ssilvestre95 <ssilvestre1995@gmail.com>
2026-04-13 19:41:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const exerciseImages = {
-    'Agachamento (com Barra ou na Smith)': {
+    'agachamento (com barra ou na smith)': {
         image: 'exercises/Barbell-Squat.gif',
         description: 'Exercício fundamental para desenvolvimento de pernas e glúteos. Mantenha as costas retas e desça até 90 graus.'
     },
